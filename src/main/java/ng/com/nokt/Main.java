2bd97df403afad11d6ca864ea96a2c30ad4e9be3
BarkaBoss <bossmajor.biii@gmail.com>
2024-03-12 12:42:13 +0100

    Add Bean Manually Register Bean

--- src/main/java/ng/com/nokt/Main.java
+++ src/main/java/ng/com/nokt/Main.java
@@ -3,13 +3,16 @@ package ng.com.nokt;
 import ng.com.nokt.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
         Parrot p = context.getBean(Parrot.class);
-        System.out.println(p);
-        p.setName("Parry");//#033761
+        //System.out.println(p);
+        p.setName("Parry");
+        Supplier<Parrot> parrotSupplier = ()-> p;
         System.out.println(p.getName());
 
     }
