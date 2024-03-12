05719f4e5008c3b9fdbc0499f1b247fd2c284ecf
BarkaBoss <bossmajor.biii@gmail.com>
2024-03-12 10:50:28 +0100

    Marked main Bean as Primary

--- src/main/java/ng/com/nokt/config/ProjectConfig.java
+++ src/main/java/ng/com/nokt/config/ProjectConfig.java
@@ -3,10 +3,12 @@ package ng.com.nokt.config;
 import ng.com.nokt.Parrot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ProjectConfig {
 
+    @Primary
     @Bean(name = "lele")
     Parrot parrot(){
         var p = new Parrot();
